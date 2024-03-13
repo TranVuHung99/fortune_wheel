fdbc78c04491745682c963fa870a2f8073e3a310
Ngô Tài Cơ <cont@suga.com.vn>
2024-03-13 07:45:36 +0000

    Resolve "Add Wheel Table"

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lucid_decision/go_router_config.dart';
-import 'package:lucid_decision/modules/home/app/ui/history/history_wheel_page.dart';
 import 'package:lucid_decision/injector.dart';
 import 'package:lucid_decision/theme/ui_theme.dart';
 
